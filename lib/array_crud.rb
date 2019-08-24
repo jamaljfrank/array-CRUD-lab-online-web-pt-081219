3d2981223f9315d0462ea787d21f6af81e5a59de
Jamal Frank <jamaljfrank@gmail.com>
2019-08-24 15:18:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  a = []
 end
 
 def create_an_array
-  
+  a = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
